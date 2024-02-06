308f536837ef97ca10c530ab232075fcd6d26d79
Miguel Andrey de barros silva <115722421+M1guelAndrey@users.noreply.github.com>
2024-02-05 23:15:38 -0300

    atualização JogoTabuleiro.java

--- lpoo_CampoMinado/src/game/campominado/tabuleiro/JogoTabuleiro.java
+++ lpoo_CampoMinado/src/game/campominado/tabuleiro/JogoTabuleiro.java
@@ -1,5 +1,23 @@
 package game.campominado.tabuleiro;
 
-public interface JogoTabuleiro {
+import game.campominado.celula.Celula;
 
+public interface JogoTabuleiro {
+    int getLinhas();
+
+    int getColunas();
+
+    int getBombas();
+
+    Celula[][] getCelulas();
+
+    void setCelulas(Celula[][] celulas);
+
+    void iniciarCampo();
+
+    int continuandoJogo(int linha, int coluna, int id);
+
+    void abrirVizinhas(int linha, int coluna);
+
+    void addMinas();
 }
