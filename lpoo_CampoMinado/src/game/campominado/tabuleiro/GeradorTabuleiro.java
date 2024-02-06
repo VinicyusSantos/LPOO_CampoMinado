42bcfa5e49e1bccda16497cfa00b3c7cf2d77db2
Miguel Andrey de barros silva <115722421+M1guelAndrey@users.noreply.github.com>
2024-02-05 23:15:00 -0300

    atualização GeradorTabuleiro.java

--- lpoo_CampoMinado/src/game/campominado/tabuleiro/GeradorTabuleiro.java
+++ lpoo_CampoMinado/src/game/campominado/tabuleiro/GeradorTabuleiro.java
@@ -4,17 +4,31 @@ public class GeradorTabuleiro {
 	private int linhas;
 	private int colunas;
 	private int bombas;
+	private int dificuldade;
 
-	public GeradorTabuleiro(int linhas, int colunas, int bombas) {
+	public GeradorTabuleiro(int linhas, int colunas, int bombas, int dificuldade) {
 		this.linhas = linhas;
 		this.colunas = colunas;
 		this.bombas = bombas;
+		this.dificuldade = dificuldade;
 	}
 
 	public Tabuleiro gerarTabuleiro() {
-		Tabuleiro tab = new Tabuleiro(linhas, colunas, bombas);
-		tab.iniciarCampo();
-		return tab;
+		if(dificuldade == 1) {
+			TabuleiroIniciante tab = new TabuleiroIniciante(linhas, colunas, bombas);
+			tab.iniciarCampo();
+			return tab;
+		}
+		else if(dificuldade == 2) {
+			TabuleiroIntermediario tab = new TabuleiroIntermediario(linhas, colunas, bombas);
+			tab.iniciarCampo();
+			return tab;
+		}
+		else {
+			TabuleiroAvancado tab = new TabuleiroAvancado(linhas, colunas, bombas);
+			tab.iniciarCampo();
+			return tab;
+		}
 	}
 
 }
