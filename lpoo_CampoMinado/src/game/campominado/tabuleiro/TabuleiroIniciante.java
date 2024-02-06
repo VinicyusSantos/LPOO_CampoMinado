4193e9cf00f79c48791397a16c2e90718f553611
Miguel Andrey de barros silva <115722421+M1guelAndrey@users.noreply.github.com>
2024-02-05 23:17:11 -0300

    atualização TabuleiroIniciante.java

--- lpoo_CampoMinado/src/game/campominado/tabuleiro/TabuleiroIniciante.java
+++ lpoo_CampoMinado/src/game/campominado/tabuleiro/TabuleiroIniciante.java
@@ -1,5 +1,50 @@
 package game.campominado.tabuleiro;
 
-public class TabuleiroIniciante {
+import game.campominado.celula.Celula;
+import game.campominado.celula.EspacoVazio;
 
+public class TabuleiroIniciante extends Tabuleiro {
+
+    public TabuleiroIniciante(int linhas, int colunas, int bombas) {
+        super(linhas, colunas, bombas);
+        iniciarCampo();
+    }
+
+    @Override
+    public void iniciarCampo() {
+    	Celula[][] celulas = getCelulas();
+        celulas = new Celula[getLinhas()][getColunas()];
+        setCelulas(celulas);  
+        for (int i = 0; i < getLinhas(); i++) {
+            for (int j = 0; j < getColunas(); j++) {
+                celulas[i][j] = new EspacoVazio();
+            }
+        }
+        // adicionando vizinhos para verificar a qts de bombas
+        for (int i = 0; i < getLinhas(); i++) {
+            for (int j = 0; j < getColunas(); j++) {
+                if (i > 0) {
+                    if (j > 0)
+                        celulas[i][j].addVizinhos(celulas[i - 1][j - 1]);
+                    celulas[i][j].addVizinhos(celulas[i - 1][j]);
+                    if (j < getColunas() - 1)
+                        celulas[i][j].addVizinhos(celulas[i - 1][j + 1]);
+                }
+
+                if (j > 0)
+                    celulas[i][j].addVizinhos(celulas[i][j - 1]);
+                if (j < getColunas() - 1) // Corrigido aqui
+                    celulas[i][j].addVizinhos(celulas[i][j + 1]);
+
+                if (i < getLinhas() - 1) {
+                    if (j > 0)
+                        celulas[i][j].addVizinhos(celulas[i + 1][j - 1]);
+                    celulas[i][j].addVizinhos(celulas[i + 1][j]);
+                    if (j < getColunas() - 1)
+                        celulas[i][j].addVizinhos(celulas[i + 1][j + 1]);
+                }
+            }
+        }
+        addMinas();
+    }
 }
