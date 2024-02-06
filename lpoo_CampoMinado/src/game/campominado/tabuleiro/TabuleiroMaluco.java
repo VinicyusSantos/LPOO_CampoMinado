9871e5b9cc9a45d14daac0d6608b69ca41c15ae6
Miguel Andrey de barros silva <115722421+M1guelAndrey@users.noreply.github.com>
2024-02-05 23:18:14 -0300

    atualização TabuleiroMaluco.java

--- lpoo_CampoMinado/src/game/campominado/tabuleiro/TabuleiroMaluco.java
+++ lpoo_CampoMinado/src/game/campominado/tabuleiro/TabuleiroMaluco.java
@@ -7,67 +7,95 @@ import game.campominado.celula.Celula;
 import game.campominado.celula.CelulaMaluca;
 
 public class TabuleiroMaluco extends Tabuleiro {
-	private double nivelMaluquice;
+    private double nivelMaluquice;
 
-	public TabuleiroMaluco(int linhas, int colunas, int bombas, double nivelMaluquice) {
-		super(linhas, colunas, bombas);
-		this.nivelMaluquice = nivelMaluquice;
-		iniciarCampo();
-	}
+    public TabuleiroMaluco(int linhas, int colunas, int bombas, double nivelMaluquice) {
+        super(linhas, colunas, bombas);
+        this.nivelMaluquice = nivelMaluquice;
+        iniciarCampo();
+    }
 
-	@Override
-	public void addMinas() {
-		Random random = new Random();
-		int minasRestantes = getBombas();
+    @Override
+    public void addMinas() {
+        Random random = new Random();
+        int minasRestantes = getBombas();
 
-		while (minasRestantes > 0) {
-			int linha = random.nextInt(getLinhas());
-			int coluna = random.nextInt(getColunas());
+        while (minasRestantes > 0) {
+            int linha = random.nextInt(getLinhas());
+            int coluna = random.nextInt(getColunas());
 
-			if (!(getCelulas()[linha][coluna] instanceof Bomba)) {
-				getCelulas()[linha][coluna] = new CelulaMaluca(nivelMaluquice);
-				minasRestantes--;
-			}
-		}
+            if (!(getCelulas()[linha][coluna] instanceof Bomba)) {
+                getCelulas()[linha][coluna] = new CelulaMaluca(nivelMaluquice);
+                minasRestantes--;
+            }
+        }
 
-		// Adicionando vizinhos para as bombas
-		for (int i = 0; i < getLinhas(); i++) {
-			for (int j = 0; j < getColunas(); j++) {
-				if (getCelulas()[i][j] instanceof Bomba) {
-					for (int x = i - 1; x <= i + 1; x++) {
-						for (int y = j - 1; y <= j + 1; y++) {
-							if (x >= 0 && x < getLinhas() && y >= 0 && y < getColunas() && !(x == i && y == j)) {
-								getCelulas()[x][y].addVizinhos(getCelulas()[i][j]);
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+        // Adicionando vizinhos para as bombas
+        for (int i = 0; i < getLinhas(); i++) {
+            for (int j = 0; j < getColunas(); j++) {
+                if (getCelulas()[i][j] instanceof Bomba) {
+                    // Adiciona todos os vizinhos para as bombas
+                    for (int x = i - 1; x <= i + 1; x++) {
+                        for (int y = j - 1; y <= j + 1; y++) {
+                            if (x >= 0 && x < getLinhas() && y >= 0 && y < getColunas() && !(x == i && y == j)) {
+                                getCelulas()[x][y].addVizinhos(getCelulas()[i][j]);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-	// Reiniciar o tabuleiro maluco
-	public void reiniciarTabuleiro() {
-		iniciarCampo();
-		addMinas();
-	}
+    
+    public void reiniciarTabuleiro() {
+        iniciarCampo();
+        addMinas();
+    }
+    
 
-	@Override
-	public void iniciarCampo() {
-		setCelulas(new Celula[getLinhas()][getColunas()]);
-		Random random = new Random();
+    @Override
+    public void abrirVizinhas(int linha, int coluna) {
+        Celula celula = getCelulas()[linha][coluna];
 
-		for (int i = 0; i < getLinhas(); i++) {
-			for (int j = 0; j < getColunas(); j++) {
-				double probabilidadeBomba = random.nextDouble();
+        if (celula.isAberto()) {
+            return;
+        }
 
-				if (probabilidadeBomba < nivelMaluquice) {
-					getCelulas()[i][j] = new CelulaMaluca(nivelMaluquice);
-					getCelulas()[i][j].addBomba();
-				} else {
-					getCelulas()[i][j] = new CelulaMaluca(nivelMaluquice);
-				}
-			}
-		}
-	}
+        int resultado = celula.clique();
+
+        if (resultado != -1 && celula.numeroBombasVizinhas() == 0) {
+            for (int i = linha - 1; i <= linha + 1; i++) {
+                for (int j = coluna - 1; j <= coluna + 1; j++) {
+                    if (i >= 0 && i < getLinhas() && j >= 0 && j < getColunas()) {
+
+                        if (i != linha || j != coluna) {
+                            abrirVizinhas(i, j);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+ 
+
+    @Override
+    public void iniciarCampo() {
+        setCelulas(new Celula[getLinhas()][getColunas()]);
+        Random random = new Random();
+
+        for (int i = 0; i < getLinhas(); i++) {
+            for (int j = 0; j < getColunas(); j++) {
+                double probabilidadeBomba = random.nextDouble();
+
+                if (probabilidadeBomba < nivelMaluquice) {
+                    getCelulas()[i][j] = new CelulaMaluca(nivelMaluquice);
+                    getCelulas()[i][j].addBomba();
+                } else {
+                    getCelulas()[i][j] = new CelulaMaluca(nivelMaluquice);
+                }
+            }
+        }
+    }
 }
