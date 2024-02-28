ee6fdcb6f20c9131c36df4899b507588204f008d
Vinicyus Santos <103897032+VinicyusSantos@users.noreply.github.com>
2024-02-28 08:13:06 -0300

    Update Jogador.java

--- lpoo_CampoMinado/src/game/campominado/jogador/Jogador.java
+++ lpoo_CampoMinado/src/game/campominado/jogador/Jogador.java
@@ -1,5 +1,11 @@
 package game.campominado.jogador;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Jogador {
 	private String name;
 	private int xp;
@@ -21,6 +27,25 @@ public class Jogador {
 		this.xp = xp;
 	}
 
-	
+	// Método para salvar informações do jogador em um arquivo
+    public void salvarInfo(String arquivo) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo))) {
+            writer.write(getName() + "," + getXp());
+        }
+    }
+
+    // Método para carregar informações do jogador de um arquivo
+    public static Jogador carregarInfo(String arquivo) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(arquivo))) {
+            String linha = reader.readLine();
+            if (linha != null) {
+                String[] partes = linha.split(",");
+                String nome = partes[0];
+                int xp = Integer.parseInt(partes[1]);
+                return new Jogador(nome, xp);
+            }
+        }
+        return null;
+    }
 	
 }
